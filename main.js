0e157e4123344ef438326a571cbd09c2dce8540d
ubai-d <ubaidurrehman14827@gmail.com>
2024-03-20 09:56:46 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var isRaining = false;
+var heavyRain = true;
+if (isRaining) {
+    console.log("Yes It's Raining Wear Raincoat");
+}
+else if (heavyRain) {
+    console.log("sit back to your home");
+}
+else {
+    console.log('Wear Sunglasses');
+}
---
+++ main.ts
@@ -0,0 +1,12 @@
+let isRaining = false;
+let heavyRain = true
+if(isRaining){
+    console.log("Yes It's Raining Wear Raincoat");
+}
+else if(heavyRain){
+    console.log("sit back to your home");
+    
+}
+else{
+    console.log('Wear Sunglasses');
+}
